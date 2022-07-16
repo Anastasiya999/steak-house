003ef865ef6e4eeef0d30d273e224cfaa324bca5
Anastasiya999 <anapechko@gmail.com>
2022-07-16 23:03:43 +0200

    adding autoscroll

--- src/components/LogoBar/index.jsx
+++ src/components/LogoBar/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import styles from "./LogoBar.module.scss";
 
 export default function LogoBar({ logos }) {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  if (isTabletOrMobile)
+    return (
+      <marquee width="100%" direction="left" scrollamount={6}>
+        <ul className={styles.root}>
+          {logos.map((item, idx) => (
+            <li key={idx}>
+              <img src={item} />
+            </li>
+          ))}
+        </ul>
+      </marquee>
+    );
   return (
     <ul className={styles.root}>
       {logos.map((item, idx) => (
